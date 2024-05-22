3fb284f7418e276f8ab91fc4ac0220d91d7edb9c
Ramesh Gyawali <gamingrowmexh@gmail.com>
2024-05-22 16:53:13 +0545

    added signup function and login function

--- app.py
+++ app.py
@@ -78,12 +78,12 @@ def developer():
 
 @app.route('/login.html')
 @cache.cached(timeout=3600)
-def developer():
+def login():
     return render_template("login.html")
 
 @app.route('/signup.html')
 @cache.cached(timeout=3600)
-def developer():
+def signup():
     return render_template("signup.html")
 
 @app.route('/blog.html')
